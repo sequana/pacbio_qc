ebfc3bbfaba7a5d6e53d151f24a1e67e0208952b
Thomas Cokelaer <cokelaer@gmail.com>
2020-12-01 14:14:57 +0100

    update metadata

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ metainfo = {
     'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
     'keywords' : ['pacbio, snakemake, NGS, sequana'],
     'classifiers' : [
-          'Development Status :: 4 - Beta',
-          #'Development Status :: 5 - Production/Stable',
+          #'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Education',
           'Intended Audience :: End Users/Desktop',
           'Intended Audience :: Developers',
