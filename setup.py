5837a0d4e34754540def627caa54bb878e3087f0
Thomas Cokelaer <cokelaer@gmail.com>
2022-01-05 23:57:38 +0100

    update setup to install sequana_pipetools

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ metainfo = {
     'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
     'keywords' : ['pacbio, snakemake, NGS, sequana'],
     'classifiers' : [
-          #'Development Status :: 4 - Beta',
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Education',
           'Intended Audience :: End Users/Desktop',
@@ -26,8 +25,9 @@ metainfo = {
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
           'Topic :: Scientific/Engineering :: Information Analysis',
@@ -52,16 +52,15 @@ setup(
     classifiers      = metainfo['classifiers'],
 
     # package installation
-    packages = ["sequana_pipelines.pacbio_qc",
-        'sequana_pipelines.pacbio_qc.data' ],
+    packages = ["sequana_pipelines.pacbio_qc"],
 
-    install_requires = "sequana",
+    install_requires = open("requirements.txt").read(),
 
     # This is recursive include of data files
     exclude_package_data = {"": ["__pycache__"]},
     package_data = {
-        '': ['*.yaml', "*.rules", "*.json", "requirements.txt"],
-        'sequana_pipelines.pacbio_qc.data' : ['*.*'], 
+        '': ['*.yaml', "*.rules", "*.json", "requirements.txt", "*png"],
+
         },
 
     zip_safe=False,
