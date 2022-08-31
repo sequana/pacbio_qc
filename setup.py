c16eadee0dacc08c468022e6c2a0dc01aea18bf6
Thomas Cokelaer <cokelaer@gmail.com>
2022-08-31 08:35:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 _MAJOR               = 0
-_MINOR               = 10
+_MINOR               = 11
 _MICRO               = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
